446235cf5440ac0bdfabbf005286ad12df2bfbd3
Pedro Crisanto <pedrohcrisanto@gmail.com>
2019-03-14 14:35:35 -0300

    terceiro commit

--- config/database.rb
+++ config/database.rb
@@ -9,13 +9,15 @@ configure :test do
     }
   end
      
-  configure :development do
+  configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/onebitbot_production')
+  
     set :database, {
-      adapter: 'postgresql',
-      encoding: 'utf8',
-      database: 'onebitbot_development',
-      pool: 5,
-      username: 'postgres',
-      host: 'postgres'
+      adapter:  'postgresql',
+      host:     db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
     }
   end
